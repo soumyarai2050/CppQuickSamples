90a03d7116d654f1a831ac6ae2d3f81b9cc39ad4
soumyarai2050 <soumyarai2050@gmail.com>
2021-07-29 15:05:39 +0800

    Base impl of LogicalProblemSimpleFunction.cpp

---
+++ LogicalProblemSimpleFunction.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+//Problem : Reverse a string
+std::string ReverseString(const std::string &name)
+{
+    return name;
+}
+
+
+bool Validate_ReverseString()
+{
+    bool testStatus = false;
+    //Sanity Test
+    std::string name ("Sumit");
+    if("timuS" == ReverseString(name))
+    {
+        std::cout << "Sanity Test - Pass\n";
+        testStatus &= true;
+    }
+    else
+    {
+        std::cerr << "Sanity Test - Fail\n";
+        testStatus &= false;
+    }
+
+    //Clear Test
+    name.clear();
+    if("" == ReverseString(name))
+    {
+        std::cout << "Clear Test - Pass\n";
+        testStatus &= true;
+    }
+    else
+    {
+        std::cout << "Clear Test - Fail\n";
+        testStatus &= false;
+    }
+    return testStatus;
+}
+
+int main()
+{
+    std::cout << "Test Start\n" ;
+    bool testResult = Validate_ReverseString();
+    std::cout << "Overall Test status: " << (testResult?"Pass":"Fail");
+    return 0;
+}
