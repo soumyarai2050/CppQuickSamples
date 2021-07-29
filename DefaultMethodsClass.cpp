c25c4eb9bd4d98d35c3071e31c857c8ac511b08d
soumyarai2050 <soumyarai2050@gmail.com>
2021-07-29 22:26:36 +0800

    Updated DefaultMethodsClass.cpp

--- DefaultMethodsClass.cpp
+++ DefaultMethodsClass.cpp
@@ -1,21 +1,41 @@
-//
-// Created by kumar on 7/29/2021.
-//
-
 #include <iostream>
+#include <vector>
 #include <algorithm>
 
 class DefaultMethodsClass
 {
 public:
     //Default Constructor
+    DefaultMethodsClass():intVar(5),intVector{3,4}
+    {
+        std::cout << "DefaultMethodsClass Constructor\n";
+    }
+
     //Copy Constructor
+    DefaultMethodsClass(const DefaultMethodsClass &obj):intVar(obj.intVar),intVector(obj.intVector)
+    {
+        std::cout << "DefaultMethodsClass Copy Constructor\n";
+    }
+
     //Move Constructor
+    DefaultMethodsClass(DefaultMethodsClass &&obj):intVar{exchange (obj.intVar, 0)},intVector(std::move(obj.intVector))
+    {
+        std::cout << "DefaultMethodsClass Copy Constructor\n";
+    }
+
     //Assignment Operator
     //Copy Assignment Operator
-    //Move Assignment Operator
+    //Move Assignment Operator 
     //Destructor
-
-
+private:
+    int intVar;
+    std::vector<int> intVector;
 
 };
+
+int main()
+{
+    DefaultMethodsClass obj1;
+    DefaultMethodsClass obj2(obj1);
+    return 0;
+}
