dfbde0ada4e7f51d51a2a0c56eb88d7f144ee83c
soumyarai2050 <soumyarai2050@gmail.com>
2021-07-30 13:54:36 +0800

    Updated DefaultMethodsClass.cpp

--- DefaultMethodsClass.cpp
+++ DefaultMethodsClass.cpp
@@ -8,28 +8,66 @@ public:
     //Default Constructor
     DefaultMethodsClass():intVar(5),intVector{3,4}
     {
-        std::cout << "DefaultMethodsClass Constructor\n";
+        std::cout << "DefaultMethodsClass Default Constructor\n";
+        Display();
     }
 
     //Copy Constructor
     DefaultMethodsClass(const DefaultMethodsClass &obj):intVar(obj.intVar),intVector(obj.intVector)
     {
         std::cout << "DefaultMethodsClass Copy Constructor\n";
+        Display();
+        obj.Display();
     }
 
     //Move Constructor
-    DefaultMethodsClass(DefaultMethodsClass &&obj):intVar{exchange (obj.intVar, 0)},intVector(std::move(obj.intVector))
+    DefaultMethodsClass(DefaultMethodsClass&& obj):intVar{std::exchange(obj.intVar, 0)},intVector(std::move(obj.intVector))
     {
-        std::cout << "DefaultMethodsClass Copy Constructor\n";
+        std::cout << "DefaultMethodsClass Move Constructor\n";
+        Display();
+        obj.Display();
     }
 
-    //Assignment Operator
     //Copy Assignment Operator
-    //Move Assignment Operator 
+    DefaultMethodsClass& operator=(const DefaultMethodsClass& obj)
+    {
+        intVar = obj.intVar;
+        intVector = obj.intVector;
+        std::cout << "DefaultMethodsClass Copy Assignment Operator\n";
+        Display();
+        obj.Display();
+        return *this;
+    }
+
+    //Move Assignment Operator
+    DefaultMethodsClass& operator=(DefaultMethodsClass&& obj)
+    {
+        intVar = std::exchange(obj.intVar,0);
+        intVector = std::move(obj.intVector);
+        std::cout << "DefaultMethodsClass Move Assignment Operator\n";
+        Display();
+        obj.Display();
+        return *this;
+    }
+
     //Destructor
+    virtual ~DefaultMethodsClass()
+    {
+        std::cout << "DefaultMethodsClass Destructor\n";
+        intVector.clear();
+        Display();
+    }
 private:
+    void Display() const
+    {
+        std::cout << "intVar: " << intVar << " intVector: ";
+        for (cItrIntVector cItr = intVector.begin(); cItr != intVector.end(); cItr++)
+            std::cout << *cItr << " ";
+        std::cout << std::endl;
+    }
     int intVar;
     std::vector<int> intVector;
+    using cItrIntVector = std::vector<int>::const_iterator;
 
 };
 
@@ -37,5 +75,9 @@ int main()
 {
     DefaultMethodsClass obj1;
     DefaultMethodsClass obj2(obj1);
+    obj1 = obj2;
+    DefaultMethodsClass obj3 = std::move(obj1);
+    obj1 = std::move(obj2);
+
     return 0;
 }
