afa503e7edd21525ab44688a8cb18015a8ebd016
Soumya <soumyarai2050@gmail.com>
2021-08-01 17:10:46 +0800

    std::chrono with nanoseconds support

--- Chrono.cpp
+++ Chrono.cpp
@@ -8,6 +8,7 @@ int main()
     sleep(2);
     auto end = std::chrono::high_resolution_clock::now();
     //assume deltaTime < 1 ms
+    //replace below nanoseconds with std::chrono::microseconds or std::chrono::milliseconds if needed
     auto deltaTime = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count();
     std::cout << "2 sec is : " << deltaTime;
     return 0;
