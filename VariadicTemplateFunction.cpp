9a7702ecec81a5928be94f4ab14cb18c27e0e450
Soumya <soumyarai2050@gmail.com>
2021-08-06 21:12:07 +0800

    improved static_asserts in VariadicTemplateFunction Add to ensure compile time error is thrown if accidentally called with less than 2 parameters

--- VariadicTemplateFunction.cpp
+++ VariadicTemplateFunction.cpp
@@ -15,18 +15,26 @@
 template <typename U, typename T>
 auto Add(U u, T t)
 {
+    //std::cout << "is_floating_point_v<U>: " << std::boolalpha << std::is_floating_point_v<U> << " is_integral_v<U>: " << std::boolalpha << std::is_integral_v<U> << std::endl;
+    static_assert(std::is_floating_point_v<U> || std::is_integral_v<U>, "Only integral and floating types supported");
+    //std::cout << "is_floating_point_v<T>: " << std::boolalpha << std::is_floating_point_v<T> << " is_integral_v<T>: " << std::boolalpha << std::is_integral_v<T> << std::endl;
+    static_assert(std::is_floating_point_v<T> || std::is_integral_v<T>, "Only integral and floating types supported");
     return (u + t);
-};
+}
 
 template <typename U, typename... T>
 auto Add(U u, T... t)
 {
     //std::cout << "sizeof...(T): " << sizeof...(T) << std::endl;
+    //std::cout << "is_floating_point_v<U>: " << std::boolalpha << std::is_floating_point_v<U> << " is_integral_v<U>: " << std::boolalpha << std::is_integral_v<U> << std::endl;
+    static_assert(std::is_floating_point_v<U> || std::is_integral_v<U>, "Only integral and floating types supported");
     static_assert(sizeof...(T) >1, "Minimum 2 parameters required to add numbers");
     return (u + Add(t...));
-};
+}
+
 int main()
 {
-    std::cout << Add(4,5.6,6,7.3,8,9); //graceful return type with auto
+    std::cout << Add(2,5.6,6,7.3,8,9) << std::endl; //graceful return type with auto
+    std::cout << Add(4,'c') << std::endl; //graceful return type with auto
     return 0;
 }
